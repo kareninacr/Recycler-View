9234144e269eaacaee5e87077164002349128358
Karenina <kareninacasandrar76@gmail.com>
2019-08-22 11:50:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recyclerandcardview/Song.java
@@ -0,0 +1,49 @@
+package com.example.recyclerandcardview;
+
+class Song {
+    private String name, singer, year;
+    private int pic, rank;
+
+    public Song(String name, String singer, int rank, int pic) {
+        this.name = name;
+        this.singer = singer;
+        this.rank = rank;
+        this.pic = pic;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public int getPic() {
+        return pic;
+    }
+    public void setPic(int pic) {
+        this.pic = pic;
+    }
+
+    public String getYear() {
+        return year;
+    }
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
